125e5cac7bc4986c8ce5131916bef3555b1eddb1
Gguife <gguife747@gmail.com>
2024-02-01 23:58:22 -0300

    feat: Creating global styles

---
+++ src/GlobalStyle.ts
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body{
+    font-family: 'Inter', sans-serif;
+    background-color: #121214;
+    color: #FFF;
+  }
+`;
+
+export default GlobalStyle;
