74220da7bbfcd0a1386abc48b47c9724c081700e
Gguife <gguife747@gmail.com>
2024-02-03 22:35:51 -0300

    feat: Creat main page kenai

--- src/views/kenai/Index.tsx
+++ src/views/kenai/Index.tsx
@@ -1,7 +1,40 @@
+import { useState, useEffect } from "react";
+import Header from "../../components/header/Index";
+import CardMain from "../../components/cardMainKenai/Index";
+import { KenaiContainer, KenaiMoviesCard } from "./Style";
+import { MovieService } from "../../api/api";
+
+interface Movie {
+  id: number
+}
 
 const Kenai = () =>{
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const getMovies = async () =>{
+    try{
+      const {data: {results}} = await MovieService.getAllMovies();
+      setMovies(results); 
+    }catch(error){
+      console.error("Erro ao obter filmes:", error);
+    }
+  }
+
+  useEffect(() =>{
+    getMovies();
+  }, [])
+
   return(
-    <div>kenai movies</div>
+    <KenaiContainer>
+      <Header />
+      <KenaiMoviesCard>
+        {movies.map((movie) => (
+          <div key={movie.id}>
+            <CardMain movieProp={movie} />
+          </div>
+        ))}
+      </KenaiMoviesCard>
+    </KenaiContainer>
   )
 }
 
---
+++ src/views/kenai/Style.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const KenaiContainer = styled.div`
+
+`;
+
+export const KenaiMoviesCard = styled.div`
+margin: 8rem 0 2rem 0;
+display: flex;
+flex-wrap: wrap;
+gap: 3rem;
+align-items: center;
+justify-content: center;
+`;
