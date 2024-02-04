b6eb20de5e10178b67b80d8e49e5ffe58819f40d
Gguife <gguife747@gmail.com>
2024-02-03 22:34:07 -0300

    feat: Connection movies api with Axios

---
+++ src/api/api.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const BASE_URL = 'https://api.themoviedb.org/3/'
+const API_KEY = '78e75d524d54f7dc8cb20ecdd27e5dfe'
+
+const withBaseUrl = ( path: string ) => `${BASE_URL}${path}?api_key=${API_KEY}`
+
+
+
+export class MovieService {
+  static getAllMovies (){
+    return axios(withBaseUrl("movie/popular"));
+  }
+  static getMovie (id: number){
+    return axios(withBaseUrl(`movie/${id}`));
+  }
+}
