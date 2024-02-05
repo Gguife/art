f7ff80959a3dc73e11394ff3b90e3132e97a975e
Gguife <gguife747@gmail.com>
2024-02-05 17:21:34 -0300

    feat: Create dinamic route for page movie

--- src/router/MainRouter.tsx
+++ src/router/MainRouter.tsx
@@ -1,11 +1,13 @@
 import { useRoutes } from "react-router-dom";
 import Home from "../views/home/Index";
-import Kenai from "../views/kenai/index";
+import Kenai from "../views/kenai/Index.tsx";
+import MovieDetail from "../views/movieDetail/Index.tsx";
 
 export const MainRouter = () =>{
   const router = useRoutes([
     {path: "/", element: <Home />},
-    {path: "/kenai", element: <Kenai />}
+    {path: "/kenai", element: <Kenai />},
+    {path: "/movie/:id", element: <MovieDetail />}
   ])
 
   return router;
