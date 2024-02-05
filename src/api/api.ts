d7e4bc3aa472ed1a4af26167e9f39716223cd908
Gguife <gguife747@gmail.com>
2024-02-05 16:39:00 -0300

    fix: Id type

--- src/api/api.ts
+++ src/api/api.ts
@@ -5,13 +5,11 @@ const API_KEY = '78e75d524d54f7dc8cb20ecdd27e5dfe'
 
 const withBaseUrl = ( path: string ) => `${BASE_URL}${path}?api_key=${API_KEY}`
 
-
-
 export class MovieService {
   static getAllMovies (){
     return axios(withBaseUrl("movie/popular"));
   }
-  static getMovie (id: number){
+  static getMovie (id: any){
     return axios(withBaseUrl(`movie/${id}`));
   }
 }
