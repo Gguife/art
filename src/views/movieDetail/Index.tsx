8d3d62d2fdd36c00dbc93e4b6cf6ee17ef64da00
Gguife <gguife747@gmail.com>
2024-02-07 17:14:44 -0300

    feat: Finish page about movie

--- src/views/movieDetail/Index.tsx
+++ src/views/movieDetail/Index.tsx
@@ -1,10 +1,14 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { MovieService } from "../../api/api.ts";
 import Header from "../../components/header/Index.tsx"; 
 import styled from "styled-components";
-import { MovieContent } from "./Style.ts";
+import { MovieContent, MovieMainContainter, MoviesCatalogo } from "./Style.ts";
 import { IoMdStar, IoMdStarHalf } from "react-icons/io";
+import Filme1 from "../../assets/item01.png";
+import Filme2 from "../../assets/item02.png";
+import Filme4 from "../../assets/item04.png";
+import Filme5 from "../../assets/item05.png";
 
 interface Movie {
   backdrop_path: string;
@@ -16,7 +20,6 @@ interface Movie {
 const MovieDetail = () =>{
   const {id} = useParams();
   const [movie, setMovie] = useState<Movie | undefined>(undefined);
-  
   useEffect(() => {
     const fetchMovie = async () =>{
       try{  
@@ -38,28 +41,31 @@ const MovieDetail = () =>{
   return (
     <MovieContainer backdrop_path={movie.backdrop_path} >
       <Header />
-      <MovieContent>
-        <div className="stars">
-          <IoMdStar className="star-icon" />
-          <IoMdStar className="star-icon" />
-          <IoMdStar className="star-icon" />
-          <IoMdStar className="star-icon" />
-          <IoMdStarHalf className="star-icon" />
-        </div>
-        <div className="movies-info">
-          <h1>{movie.title}</h1>
-          <p>Original Leangue: <span>{movie.original_language}</span></p>
-          <p>{movie.overview}</p>
-          <button>Assistir Agora</button>
-        </div>
-        <div className="movies-catalago">
-          <p>Filme 1</p>
-          <p>Filme 2</p>
-          <p>Filme 3</p>
-          <p>Filme 4</p>
-          <p>Filme 5</p>
-        </div>
-      </MovieContent>
+      <MovieMainContainter>
+        <MovieContent>
+          <div className="movies-info">
+            <div className="stars">
+              <IoMdStar className="star-icon" />
+              <IoMdStar className="star-icon" />
+              <IoMdStar className="star-icon" />
+              <IoMdStar className="star-icon" />
+              <IoMdStarHalf className="star-icon" />
+            </div>
+            <div className="movie-content">
+              <h1>{movie.title}</h1>
+              <p className="movie-lang">Original Leangue: <span>{movie.original_language}</span></p>
+              <p className="movie-desc">{movie.overview}</p>
+            </div>
+            <Link to="/kenai" className="movie-button">Assistir Agora</Link>
+          </div>
+        </MovieContent>
+        <MoviesCatalogo>
+          <img src={Filme1} alt="" />
+          <img src={Filme2} alt="" />
+          <img src={Filme4} alt="" />
+          <img src={Filme5} alt="" />
+        </MoviesCatalogo>
+      </MovieMainContainter>
     </MovieContainer>
   )
 }
--- src/views/movieDetail/Style.ts
+++ src/views/movieDetail/Style.ts
@@ -1,10 +1,129 @@
 import styled from "styled-components";
 
-export const MovieContent = styled.div`
-position: absolute;
-z-index: 1;
-margin-top: 8rem;
+export const MovieMainContainter = styled.section`
+position: relative;
+z-index: 99;
+height: 100%;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+gap: 3rem;
+padding: 1rem;
+`;
 
-  .movie-infos{
+export const MovieContent = styled.div`
+max-width: 1100px;
+width: 100%;
+
+  .movies-info{
+    padding: 1rem;
   }
+  
+  .stars{
+    color: #FFA634;
+  }
+  
+  .movie-content{
+    margin: 2rem 0 3rem 0;
+    
+    h1{
+      font-size: 4rem;
+    }
+    
+    .movie-lang{
+      margin: 1rem 0 1rem 0;
+      
+      span{
+        font-weight: 700;
+        color: #FFA634;
+      }
+    }
+    
+    .movie-desc{
+      max-width: 800px;
+      font-size: 1.5rem;
+    }
+  }
+  
+  .movie-button{
+    text-decoration: none;
+    color: #000;
+    background-color: #FFA634;
+    font-weight: 700;
+    padding: 1rem 3rem;
+    border: none;
+    cursor: pointer;
+    border-radius: 5px;
+    transition: .3s;
+    
+    &:hover{
+      opacity: .6;
+    }
+  }
+
+
+@media (max-width: 700px){
+  height: 80%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  
+  .movie-content{
+    margin: 0 0 8rem 0;
+  }
+
+  .movie-content{
+    margin: .5rem 0 3rem 0;
+    
+    h1{
+      font-size: 2rem;
+    }
+    
+    .movie-lang{
+      margin: 2rem 0 .5rem 0;
+      
+    }
+    
+    .movie-desc{
+      font-size: 1rem;
+    }
+  }
+}
+`;
+  
+export const MoviesCatalogo = styled.div`
+display: flex;
+gap: 3rem;
+max-width: 1100px;
+width: 100%;
+padding: 1rem;
+
+  img{
+    max-width: 150px;
+    width: 100%;
+    border-radius: 5px;
+    cursor: pointer;
+    box-shadow: rgba(0, 0, 0, 0.56) 0px 22px 70px 4px;
+    transition: all ease 0.3s;
+  
+    &:hover {
+        transform: scale(1.05); 
+      }
+  }
+
+
+@media (max-width: 790px){
+margin-top: 2rem;
+gap: 2rem;
+  img{
+    width: 140px;
+  }
+}
+
+@media (max-width: 700px){
+display: none;
+}
+
 `;
